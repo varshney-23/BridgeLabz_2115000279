7bc03f31005ce3e28e6395ce727c8ed5a422539f
Ayushi Varshney <ayushivarshney40@gmail.com>
2025-02-04 12:03:44 +0530

    extra deleted

--- Assignment-2/level-1/DistanceConverter.java
+++
@@ -1,8 +0,0 @@
-public class DistanceConverter {
-    public static void main(String[] args) {
-        double kilometers = 10.8;
-        double miles = kilometers / 1.6;
-
-        System.out.println("The distance " + kilometers + " km in miles is " + miles);
-    }
-}
